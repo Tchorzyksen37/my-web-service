bb721e54b85ead87e62658b6e4ec4bd4c0ecc91d
Tchorzyksen <evife2@gmail.com>
2020-04-03 23:51:28 +0200

    Token expiration time shortened

--- src/main/java/tchorzyksen/security/SecurityConstants.java
+++ src/main/java/tchorzyksen/security/SecurityConstants.java
@@ -3,7 +3,7 @@ package tchorzyksen.security;
 import tchorzyksen.SpringApplicationContext;
 
 public class SecurityConstants {
-    public static final long EXPIRATION_TIME = 864000000;
+    public static final long EXPIRATION_TIME = 1_600_000L;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/user";
